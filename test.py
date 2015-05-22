cfa2b9270aab6a4e19b344eb809e0fed43ff2821
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-22 10:34:15 +0530

    Updated few files

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
-shopping_list = ["banana", "orange", "apple"]
+import urllib2
+import simplejson
+import random
+import time
+import io
+movie_json = {'Plot': "A chronicle of one woman's 1,100-mile solo hike undertaken as a way to recover from a recent catastrophe.", 'Rated': 'R', 'Response': 'True', 'Language': 'English', 'Title': 'Wild', 'Country': 'USA', 'Writer': 'Nick Hornby, Cheryl Strayed (memoir "Wild: From Lost to Found on the Pacific Crest Trail")', 'Metascore': '76', 'imdbRating': '7.2', 'Director': u'Jean-Marc Vall\xe9e', 'Released': '19 Dec 2014', 'Actors': 'Reese Witherspoon, Laura Dern, Thomas Sadoski, Keene McRae', 'Year': '2014', 'Genre': 'Biography, Drama', 'Awards': 'Nominated for 2 Oscars. Another 8 wins & 48 nominations.', 'Runtime': '115 min', 'Type': 'movie', 'Poster': 'http://ia.media-imdb.com/images/M/MV5BMTczNzI2MDc1Ml5BMl5BanBnXkFtZTgwOTU5NTYxMjE@._V1_SX300.jpg', 'imdbVotes': '41294', 'imdbID': 'tt2305051'}
 
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
-}
+movie_file = open("Movie_listings_2014.csv", "w")
 
-prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
+def encode(text):
+    return text.encode('utf-8')
 
-# Write your code below!
-def compute_bill(food):
-    total = 0
-    for item in food:
-        check_stock = stock[item]
-        if check_stock > 0:
-            cost = prices[item]
-            total += cost
-            stock[item] -= 1
-    return total
+# Write the movie information to file
+title = movie_json["Title"]
+year = movie_json["Year"]
+released = movie_json["Released"].split(" ")[1]
+language = movie_json["Language"]
+rated = movie_json["Rated"]
+#imdb_rating = movie_json["imdbRating"]
+runtime = movie_json["Runtime"]
+#writer = movie_json["Writer"].split(",")[0]
+director = movie_json["Director"]
+#awards = movie_json["Awards"]
+imdb_ID = movie_json["imdbID"]
+#imdbVotes = movie_json["imdbVotes"]
 
+movie_file.write(title+",")
+movie_file.write(year+",")
+movie_file.write(encode(director)+",")
 
-print stock
-print compute_bill(["apple", "banana", "pear"])
-print stock
+movie_file.close()
