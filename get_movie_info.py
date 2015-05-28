09c58fcc6485ac54e299f2c1c5b35d0dbc6145b4
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-28 11:53:17 +0530

    First movie info for 2014

--- get_movie_info.py
+++ get_movie_info.py
@@ -24,14 +24,14 @@ for i in range(len(a)):
         movies.append(a[i].replace(" ", "+"))
 
 movie_year = '2014'
-movie_file = open("Movie_listings_2014.csv", "w")
+movie_file = open("Movie_listings_2014_all.csv", "w")
 
 def encode(text):
     return text.encode('utf-8')
 
 count = 0
 #for movie_name in movies:
-for i in range(15,16):
+for i in range(0,300):
     movie_name = movies[i]
     count += 1
     print "Doing movie no: ", count, "Movie: ", movie_name
@@ -45,23 +45,26 @@ for i in range(15,16):
     #print movie_json
 
     # Write the movie information to file
-    title = encode(movie_json["Title"])
-    year = movie_json["Year"]
-    released = movie_json["Released"]
-    language = (movie_json["Language"].split(","))[0]
-    rated = movie_json["Rated"]
-    imdb_rating = movie_json["imdbRating"]
-    runtime = (movie_json["Runtime"].split(" "))[0]
-    writer = encode(movie_json["Writer"].split(",")[0])
-    director = encode(movie_json["Director"])
-    awards = movie_json["Awards"]
-    imdb_ID = movie_json["imdbID"]
-    imdbVotes = movie_json["imdbVotes"]
-    genre = movie_json["Genre"].replace(",", " ")
+    if movie_json['Response'] == 'False':
+        movie_file.write("{name:s},,,,,,,,,,,,\n".format(name=movie_name))
+    else:
+        title = encode(movie_json["Title"])
+        year = movie_json["Year"]
+        released = movie_json["Released"]
+        language = (movie_json["Language"].split(","))[0]
+        rated = movie_json["Rated"]
+        imdb_rating = movie_json["imdbRating"]
+        runtime = (movie_json["Runtime"].split(" "))[0]
+        writer = encode(movie_json["Writer"].replace(",", " "))
+        director = encode(movie_json["Director"].replace(",", " "))
+        awards = movie_json["Awards"]
+        imdb_ID = movie_json["imdbID"]
+        imdbVotes = movie_json["imdbVotes"]
+        genre = movie_json["Genre"].replace(",", " ")
 
-    movie_file.write("{title:s}, {yr:s}, {mn:s}, {lang:s}, {time:s}, {writ:s}, {dir:s}, {rate:s}, {rated:s}, {id:s}, {vote:s}, {gen:s}, {awrds:s}\n"
-        .format(title=title, yr=year, mn=released, lang=language, time=runtime, writ=writer,
-                dir=director, rate=imdb_rating, rated = rated, id=imdb_ID, vote=imdbVotes, awrds=awards, gen=genre))
+        movie_file.write("{title:s}, {yr:s}, {mn:s}, {lang:s}, {time:s}, {writ:s}, {dir:s}, {rate:s}, {rated:s}, {id:s}, {vote:s}, {gen:s}, {awrds:s}\n"
+            .format(title=title, yr=year, mn=released, lang=language, time=runtime, writ=writer,
+                    dir=director, rate=imdb_rating, rated = rated, id=imdb_ID, vote=imdbVotes, awrds=awards, gen=genre))
     # Set up random wait:
     wait_time = random.randint(1,5)
     print "Waiting for...", wait_time
